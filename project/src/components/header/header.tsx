da795ff35b7618e6de69bc9689ed21ab332c0ec2
Alena Karenina <karena1q84@gmail.com>
2022-08-12 22:53:56 +0500

    создаст компонент UserBlock

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import Logo from '../logo/logo';
+import UserBlock from '../user-block/user-block';
 
 function Header(): JSX.Element {
   return (
@@ -6,21 +7,7 @@ function Header(): JSX.Element {
 
       <Logo />
 
-      <ul className="user-block">
-        <li className="user-block__item">
-          <div className="user-block__avatar">
-            <img
-              src="img/avatar.jpg"
-              alt="User avatar"
-              width="63"
-              height="63"
-            />
-          </div>
-        </li>
-        <li className="user-block__item">
-          <a className="user-block__link" href="/">Sign out</a>
-        </li>
-      </ul>
+      <UserBlock/>
     </header>
   );
 }
